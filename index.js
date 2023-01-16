110b674464a4d4eecc9a2d0347f10383d6d947f9
peacetoyou <49427719+peacetoyou@users.noreply.github.com>
2023-01-16 23:11:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -11,16 +11,16 @@ app.get('/', function (req, res) {
 
 app.get('/sound/:name', (req, res) => {
     const { name } = req.params
-
-    if(name == 'dog'){
-        res.json({'sound':'멍멍'})
-    } else if(name == 'cat'){
-        res.json({'sound':'야옹'})
-    } else if(name == 'pig'){
-        res.json({'sound':'꿀꿀'})
-    } else {
-        res.json({'sound':'알수없음'})
-    }
+    res.json({'sound':name})
+    // if(name == 'dog'){
+    //     res.json({'sound':'멍멍'})
+    // } else if(name == 'cat'){
+    //     res.json({'sound':'야옹'})
+    // } else if(name == 'pig'){
+    //     res.json({'sound':'꿀꿀'})
+    // } else {
+    //     res.json({'sound':'알수없음'})
+    // }
   })
 
 app.listen(port, function () {
